b79ac99fa298d45c4c08d22eab725bd59be36ad6
Nick <121521386+nick-urb@users.noreply.github.com>
2023-06-20 22:35:42 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,14 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Hero from '@components/Hero'
+import Newsletter from '@components/Newsletter'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
+    <div className="flex flex-col min-h-screen">
+      <Hero />
+      <Newsletter />
       <Footer />
     </div>
   )
